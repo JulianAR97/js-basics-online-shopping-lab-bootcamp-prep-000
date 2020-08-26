4651380cc9dacef3012ceeaf9236df7dccbbd512
whimsical-logger-3153 <julianrosenthal@yahoo.com>
2020-08-26 00:53:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,11 +16,27 @@ function addToCart(item) {
  item.itemPrice = price;
 
  cart.push(item);
- return; 
+ return;
 }
 
 function viewCart() {
   // write your code here
+  if (cart.length === 0) {
+    return 'Your shopping cart is empty.'
+  }
+  var cartStr = 'In your cart, you have ';
+
+  for (var i = 0; i < cart.length; i++) {
+    var cartEle = cart[i];
+    if (i === 0 && cart.length === 1) {
+      cartStr += `${cartEle.itemName} at $${cartEle.itemPrice}.`
+    } else if (i !== cart.length - 1){
+      cartStr += `${cartEle.itemName} at $${cartEle.itemPrice}, `
+    } else {
+      cartStr += `and ${cartEle.itemName} at $${cartEle.itemPrice}.`
+    }
+    }
+  return cartStr; 
 }
 
 function total() {
